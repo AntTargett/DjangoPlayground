5afc99156a1a757c3bc12662b57d581216e7583a
Antony Targett <antony.targett@gmail.com>
2018-07-23 21:33:28 +1000

    fix: Updated templates directory

--- Django_Project/first_app/views.py
+++ Django_Project/first_app/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def index(request):
     my_test_dictionary = {"insert_text": "I am a piece of text plz dun h8 meh"}
-    return render(request, 'index.html', context=my_test_dictionary)
+    return render(request, 'first_app/index.html', context=my_test_dictionary)
